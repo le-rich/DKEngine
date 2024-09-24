c06180466c58c141c5e9a922b536ff0f9727449c
Kevin Baumann <baumannkev@gmail.com>
2024-09-24 13:43:57 -0700

    Pulled dev branch, create MenuBar, added .h files

---
+++ UI/MenuBar.h
@@ -0,0 +1,58 @@
+#pragma once
+#ifndef MENU_BAR_H
+#define MENU_BAR_H
+
+#include "imgui.h"
+
+void createMenuBar() {
+    if (ImGui::BeginMainMenuBar()) {
+        if (ImGui::BeginMenu("File")) {
+            if (ImGui::MenuItem("New")) {
+                // Action for "New" menu item
+            }
+            if (ImGui::MenuItem("Open", "Ctrl+O")) {
+                // Action for "Open"
+            }
+            if (ImGui::MenuItem("Save", "Ctrl+S")) {
+                // Action for "Save"
+            }
+            if (ImGui::MenuItem("Exit")) {
+                // Action for "Exit"
+            }
+            ImGui::EndMenu();
+        }
+
+        if (ImGui::BeginMenu("Edit")) {
+            if (ImGui::MenuItem("Undo", "Ctrl+Z")) {
+                // Action for "Undo"
+            }
+            if (ImGui::MenuItem("Redo", "Ctrl+Y")) {
+                // Action for "Redo"
+            }
+            if (ImGui::MenuItem("Cut", "Ctrl+X")) {
+                // Action for "Cut"
+            }
+            if (ImGui::MenuItem("Copy", "Ctrl+C")) {
+                // Action for "Copy"
+            }
+            if (ImGui::MenuItem("Paste", "Ctrl+V")) {
+                // Action for "Paste"
+            }
+            ImGui::EndMenu();
+        }
+
+        if (ImGui::BeginMenu("View")) {
+            if (ImGui::MenuItem("Toggle Fullscreen")) {
+                // Action for toggling fullscreen
+            }
+            if (ImGui::MenuItem("Show/Hide Dockspace")) {
+                // Action for showing/hiding dockspace
+            }
+            ImGui::EndMenu();
+        }
+
+        ImGui::EndMainMenuBar();
+    }
+}
+
+#endif // MENU_BAR_H
--- UI/main.cpp
+++ UI/main.cpp
@@ -2,6 +2,8 @@
 #include "imgui_internal.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "Windows.h"  
+#include "MenuBar.h"  
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -11,75 +13,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-// Function to create the dockspace and additional windows
-void createImGuiWindows() {
-    // Setup a dockspace
-    ImGuiWindowFlags dockspace_flags = ImGuiWindowFlags_NoDocking;
-    ImGuiViewport* viewport = ImGui::GetMainViewport();
-
-    // Begin dockspace layout
-    ImGui::SetNextWindowPos(viewport->Pos);
-    ImGui::SetNextWindowSize(viewport->Size);
-    ImGui::SetNextWindowViewport(viewport->ID);
-    dockspace_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-    ImGui::Begin("DockSpace Demo", nullptr, dockspace_flags);
-    ImGui::PopStyleVar(2);
-
-    // Create DockSpace ID
-    ImGuiID dockspace_id = ImGui::GetID("MyDockspace");
-    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_None);
-    ImGui::End();
-
-    // First time configuration for docking layout
-    static bool firstTime = true;
-    if (firstTime) {
-        firstTime = false;
-
-        // Split the dockspace into 4 nodes (top, left, right, bottom)
-        ImGui::DockBuilderRemoveNode(dockspace_id); // Clear any previous layout
-        ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_None); // Recreate the dockspace
-        ImGuiID dock_left = ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Left, 0.25f, nullptr, &dockspace_id);
-        ImGuiID dock_right = ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Right, 0.25f, nullptr, &dockspace_id);
-        ImGuiID dock_bottom = ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Down, 0.25f, nullptr, &dockspace_id);
-        ImGuiID dock_top = ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Up, 0.25f, nullptr, &dockspace_id);
-
-        // Dock windows
-        ImGui::DockBuilderDockWindow("Viewport", dock_top);
-        ImGui::DockBuilderDockWindow("Hierarchy", dock_left);
-        ImGui::DockBuilderDockWindow("Inspector", dock_right);
-        ImGui::DockBuilderDockWindow("Browser", dock_bottom);
-        ImGui::DockBuilderDockWindow("Console", dock_bottom);
-        ImGui::DockBuilderFinish(dockspace_id); // Apply the layout
-    }
-
-    // Hierarchy window
-    ImGui::Begin("Viewport");
-    ImGui::Text("Viewport goes here");
-    ImGui::End();
-
-    // Hierarchy window
-    ImGui::Begin("Hierarchy");
-    ImGui::Text("Hierarchy goes here");
-    ImGui::End();
-
-    // Inspector window
-    ImGui::Begin("Inspector");
-    ImGui::Text("Inspector goes here");
-    ImGui::End();
-
-    // Browser window
-    ImGui::Begin("Browser");
-    ImGui::Text("Browser goes here");
-    ImGui::End();
-
-    // Console window
-    ImGui::Begin("Console");
-    ImGui::Text("Console goes here");
-    ImGui::End();
-}
 
 int main(int argc, char* argv[]) {
     std::cout << "UI PROJECT" << std::endl;
@@ -125,6 +58,9 @@ int main(int argc, char* argv[]) {
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
+
+        // Create the menu bar
+        createMenuBar();
 
         // Create the ImGui windows
         createImGuiWindows();
