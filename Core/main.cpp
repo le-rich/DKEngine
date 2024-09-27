24a0c1c4a562df409243144906de8c9fbfb57a64
Kyle N <kyle.ng7512@gmail.com>
2024-09-27 00:01:32 -0700

    [KN][COM-97] Add comments and TODO for glfw window concurrency.

--- Core/main.cpp
+++ Core/main.cpp
@@ -40,7 +40,9 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 }
 
 
-
+// TODO: Refactor Window setup to enable concurrent access to the window object for the render thread.
+// Theres some experimentation we'll need to do regarding updating objects on the main thread and rendering on the render thread.
+// That can wait for the time being.
 int run_glfw() {
 	 
 	glfwInit();
@@ -55,6 +57,10 @@ int run_glfw() {
 		glfwTerminate();
 		return -1;
 	}
+
+	// This function makes the OpenGL or OpenGL ES context of the specified window current on the calling thread.
+	// That is, only one thread can be associated with one window at a time.
+	// This'll likely be the cause of a lot of issues with rendering and ui.
 	glfwMakeContextCurrent(window);
 
 	// Load GLAD
@@ -66,6 +72,8 @@ int run_glfw() {
 
 	glViewport(0, 0, 800, 600);
 
+	// Call back for changing the viewport size of the OpenGL context.
+	// Context needs to be syncronized to caller thread.
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // primitive shapes
@@ -74,14 +82,27 @@ int run_glfw() {
 
 	Renderer renderer = Renderer();
 
+	// We want some check like this visible to the other threads
+	// That way those threads will stop once the window closes.
 	while (!glfwWindowShouldClose(window))
 	{
+		// Rendering related calls, we can move these to the loop of the rendering thread
 		glClear(GL_COLOR_BUFFER_BIT);
 		renderer.Draw(triangle); // draw tri or square
+
+		// The window has two buffers, front and back.
+		// This allows us to display the front buffer while writing to the back buffer.
+		// Once rendering is done/finished writing to the back buffer, we call this function to swap the front and back buffers
+		// This allows us to draw onto the screen without drawing on top of the previous frame.
+		// This may or may not need to be thread safe. There's conflicting information about it.
 		glfwSwapBuffers(window);
+
+		// This function processes all events in the event queue, including window and input events.
+		// Should be called in the main thread.
 		glfwPollEvents();
 	}
 
+	// Destroys library, may cause race condition if it gets destroyed while other threads are using it.
 	glfwTerminate();
 }
 
