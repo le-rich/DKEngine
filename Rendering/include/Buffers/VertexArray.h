387ed4e7d5770cca8dc99befaa4d439439484883
Lukauigi <lukebednarek@hotmail.ca>
2024-09-28 16:06:46 -0700

    [LB][COM-18] Add vertex array object & vertex buffer layout

--- Rendering/include/Buffers/VertexArray.h
+++ Rendering/include/Buffers/VertexArray.h
@@ -1,4 +1,20 @@
 #pragma once
-class VertexArray
-{};
 
+#include <glad/glad.h>
+#include "VertexBuffer.h"
+
+class VertexBufferLayout;
+
+class VertexArray
+{
+private:
+	GLuint m_RendererID;
+public:
+	VertexArray();
+	~VertexArray();
+
+	void AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout);
+
+	void Bind() const;
+	void Unbind() const;
+};
--- Rendering/include/Data/VertexBufferLayout.h
+++ Rendering/include/Data/VertexBufferLayout.h
@@ -1,1 +1,67 @@
 #pragma once
+
+#include <vector>
+#include <stdexcept>
+
+#include "Renderer.h"
+
+struct VertexBufferElement
+{
+	unsigned int type;
+	unsigned int count;
+	unsigned char normalized;
+
+	VertexBufferElement(unsigned int t, unsigned int c, bool n)
+		: type(t), count(c), normalized(n) {}
+
+	static unsigned int GetSizeOfType(unsigned int type)
+	{
+		switch (type)
+		{
+		case GL_FLOAT:			return 4;
+		case GL_UNSIGNED_INT:	return 4;
+		case GL_UNSIGNED_BYTE:	return 1;
+		}
+	}
+};
+
+class VertexBufferLayout
+{
+private:
+	std::vector<VertexBufferElement> m_Elements;
+	unsigned int m_Stride;
+
+public:
+	VertexBufferLayout()
+		: m_Stride(0) {}
+
+	template<typename T>
+	void Push(unsigned int count)
+	{
+		std::runtime_error(false);
+	}
+
+	template<>
+	void Push<float>(unsigned int count)
+	{
+		m_Elements.push_back({ GL_FLOAT, count, GL_FALSE });
+		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_FLOAT);
+	}
+
+	template<>
+	void Push<unsigned int>(unsigned int count)
+	{
+		m_Elements.push_back({ GL_UNSIGNED_INT, count, GL_FALSE });
+		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT);
+	}
+
+	template<>
+	void Push<unsigned char>(unsigned int count)
+	{
+		m_Elements.push_back({ GL_UNSIGNED_BYTE, count, GL_TRUE });
+		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE);
+	}
+
+	inline const std::vector<VertexBufferElement> GetElements() const& { return m_Elements; }
+	inline unsigned int GetStride() const { return m_Stride; }
+};
