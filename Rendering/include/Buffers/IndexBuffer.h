4fe922dfa664458a6a1671ebbd81611115da9f18
Lukauigi <lukebednarek@hotmail.ca>
2024-09-28 11:02:35 -0700

    [LB][COM-18] Add Index Buffer Implementation

--- Rendering/include/Buffers/IndexBuffer.h
+++ Rendering/include/Buffers/IndexBuffer.h
@@ -1,4 +1,19 @@
 #pragma once
+
+
 class IndexBuffer
-{};
+{
+public:
+	IndexBuffer(const void* data, unsigned int count);
+	~IndexBuffer();
+
+	void Bind() const;
+	void Unbind() const;
+
+	inline unsigned int GetCount() const { return m_Count; }
+
+private:
+	unsigned int m_RendererID;
+	unsigned int m_Count;
+};
 
--- Rendering/src/Buffers/IndexBuffer.cpp
+++ Rendering/src/Buffers/IndexBuffer.cpp
@@ -1,1 +1,25 @@
 #include "Buffers/IndexBuffer.h"
+#include "Renderer.h"
+
+
+IndexBuffer::IndexBuffer(const void* data, unsigned int count) : m_Count(count)
+{
+    GLCall(glGenBuffers(1, &m_RendererID));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID));
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW));
+}
+
+IndexBuffer::~IndexBuffer()
+{
+    GLCall(glDeleteBuffers(1, &m_RendererID));
+}
+
+void IndexBuffer::Bind() const
+{
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID));
+}
+
+void IndexBuffer::Unbind() const
+{
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+}
