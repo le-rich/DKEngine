4a34f1c1f863efb6c219c9ef2993eed2e5bdce41
Lukauigi <lukebednarek@hotmail.ca>
2024-09-29 13:19:18 -0700

    [LB][COM-18] Add struct representing vertex attributes

--- Rendering/include/Data/Vertex.h
+++ Rendering/include/Data/Vertex.h
@@ -1,4 +1,17 @@
 #pragma once
+
+#include <glm.hpp>
+
 struct Vertex
-{};
+{
+	glm::vec3 mPosition;
+	glm::vec3 mNormal;
+	glm::vec2 mTextureCoordinate;
+	glm::vec3 mTangent;
+	glm::vec3 mBitTangent;
+
+	Vertex(const glm::vec3& pos, const glm::vec3& norm, const glm::vec2& texCoord, 
+		const glm::vec3& tang, const glm::vec3& bitTang) : mPosition(pos), mNormal(norm), 
+		mTextureCoordinate(texCoord), mTangent(tang), mBitTangent(bitTang) {}
+};
 
