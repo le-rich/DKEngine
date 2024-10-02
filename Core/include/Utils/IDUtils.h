f626424dc40a1020c26b154134a50b779d7aaf08
Sepehrman <sepehrman@icloud.com>
2024-10-01 23:08:19 -0700

    [SM][com-97] moved endeaness & uuid_v4 to External

--- Core/include/Utils/IDUtils.h
+++ Core/include/Utils/IDUtils.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Utils/uuid_v4.h"
-#include "Utils/endianness.h"
+#include "../../External/uuid/uuid_v4.h"
+#include "../../External/uuid/uuid_v4.h"
 
 UUIDv4::UUIDGenerator<std::mt19937_64> uuidGen;
