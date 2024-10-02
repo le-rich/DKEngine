404ab96e006c554076b5ed27e38384b538f0a3fa
jharris99-git <jharris99@my.bcit.ca>
2024-10-01 23:48:55 -0700

    [JH][COM-99] Implemented Cable addContact

--- Physics/src/plinks.cpp
+++ Physics/src/plinks.cpp
@@ -7,3 +7,22 @@ real PointMassLink::currentLength() const
 	Vector3 relativePos = pointMass[0]->getPosition() - pointMass[1]->getPosition();
 	return relativePos.magnitude();
 }
+
+unsigned PointMassCable::addContact(PointMassContact* contact, unsigned limit) const
+{
+	real length = currentLength();
+
+	if (length < maxLength) return 0;
+
+	contact->pointMass[0] = pointMass[0];
+	contact->pointMass[1] = pointMass[1];
+
+	Vector3 normal = pointMass[1]->getPosition() - pointMass[0]->getPosition();
+	normal.normalize();
+	contact->contactNormal = normal;
+
+	contact->penetration = length - maxLength;
+	contact->restitution = restitution;
+
+	return 1;
+}
