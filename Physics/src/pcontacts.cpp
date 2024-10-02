738d002f0bd7922ffd88004d5d91e2f02a22a615
jharris99-git <jharris99@my.bcit.ca>
2024-10-01 23:22:19 -0700

    [JH][COM-99] Master PMC functions implemented.

--- Physics/src/pcontacts.cpp
+++ Physics/src/pcontacts.cpp
@@ -1,2 +1,16 @@
 #include "../include/pcontacts.h"
 
+using namespace AE86;
+
+void PointMassContact::resolve(real duration)
+{
+	resolveVelocity(duration);
+	resolveInterpenetration(duration);
+}
+
+real PointMassContact::calculateSeparatingVelocity() const
+{
+	Vector3 relativeVelocity = pointMass[0]->getVelocity();
+	if (pointMass[1]) relativeVelocity -= pointMass[1]->getVelocity();
+	return relativeVelocity * contactNormal;
+}
