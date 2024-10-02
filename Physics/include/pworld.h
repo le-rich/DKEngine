e776f5820e1b11c20ace9adf835426c1099554d0
jharris99-git <jharris99@my.bcit.ca>
2024-10-01 23:53:56 -0700

    [JH][COM-99] Defined PointMassWorld and functions

--- Physics/include/pworld.h
+++ Physics/include/pworld.h
@@ -2,9 +2,49 @@
 
 #define PWORLD_HEADER
 
+#include "pfgen.h"
+#include "plinks.h"
+
 namespace AE86
 {
+	class PointMassWorld
+	{
+	public:
 
+		typedef std::vector<PointMass*> PointMasss;
+
+		typedef std::vector<PointMassContactGenerator*> ContactGenerators;
+
+	protected:
+
+		PointMasss pointMasss;
+
+		bool calculateIterations;
+
+		PointMassForceRegistry registry;
+
+		PointMassContactResolver resolver;
+
+		ContactGenerators contactGenerators;
+
+		PointMassContact* contacts;
+
+		unsigned maxContacts;
+
+	public:
+
+		PointMassWorld(unsigned maxContacts, unsigned iterations = 0);
+
+		~PointMassWorld();
+
+		unsigned generateContacts();
+
+		void integrate(real duration);
+
+		void runPhysics(real duration);
+
+		void startFrame();
+	};
 }
 
 #endif
