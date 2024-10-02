cf6563cf474aa50acc66e0559cca649c805968ac
jharris99-git <jharris99@my.bcit.ca>
2024-10-01 23:48:19 -0700

    [JH][COM-99] Implemented PointMassLink generic

--- Physics/src/plinks.cpp
+++ Physics/src/plinks.cpp
@@ -1,1 +1,9 @@
 #include "../include/plinks.h"
+
+using namespace AE86;
+
+real PointMassLink::currentLength() const
+{
+	Vector3 relativePos = pointMass[0]->getPosition() - pointMass[1]->getPosition();
+	return relativePos.magnitude();
+}
