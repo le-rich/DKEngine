6cbb66808df1cc0d70d0424bb38dd3e85978bb42
jharris99-git <jharris99@my.bcit.ca>
2024-10-01 23:23:48 -0700

    [JH][COM-99] Implemented velocity resolver for contacts.

--- Physics/src/pcontacts.cpp
+++ Physics/src/pcontacts.cpp
@@ -14,3 +14,45 @@ real PointMassContact::calculateSeparatingVelocity() const
 	if (pointMass[1]) relativeVelocity -= pointMass[1]->getVelocity();
 	return relativeVelocity * contactNormal;
 }
+
+void PointMassContact::resolveVelocity(real duration)
+{
+	// find separating velocity
+	real separatingVelocity = calculateSeparatingVelocity();
+
+	// check if needs resolving, exit if not necessary
+	if (separatingVelocity > 0) return;
+
+	real newSepVelocity = -separatingVelocity * restitution;
+
+	Vector3 accCausedVelocity = pointMass[0]->getAcceleration();
+	if (pointMass[1]) accCausedVelocity -= pointMass[1]->getAcceleration();
+
+	real accCausedSepVelocity = accCausedVelocity * contactNormal * duration;
+
+	if (accCausedSepVelocity < 0)
+	{
+		newSepVelocity += restitution * accCausedSepVelocity;
+
+		if (newSepVelocity < 0) newSepVelocity = 0;
+	}
+
+	real deltaVelocity = newSepVelocity - separatingVelocity;
+
+
+	real totalInverseMass = pointMass[0]->getInverseMass();  // helper function? [getTotalIM()]
+	if (pointMass[1]) totalInverseMass += pointMass[1]->getInverseMass();  // helper function? [getTotalIM()]
+
+	if (totalInverseMass <= 0) return;  // helper function? [getTotalIM()]
+
+	real impulse = deltaVelocity / totalInverseMass;
+
+	Vector3 impulsePerIMass = contactNormal * impulse;
+
+	pointMass[0]->setVelocity(pointMass[0]->getVelocity() + impulsePerIMass * pointMass[0]->getInverseMass());
+
+	if (pointMass[1])
+	{
+		pointMass[1]->setVelocity(pointMass[1]->getVelocity() + impulsePerIMass * pointMass[1]->getInverseMass());
+	}
+}
