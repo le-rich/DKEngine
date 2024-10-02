9d89f1d317bf763c563f8dc4f92732da4066cd86
jharris99-git <jharris99@my.bcit.ca>
2024-10-01 23:49:45 -0700

    [JH][COM-99] Implemented Rod addContact.

--- Physics/src/plinks.cpp
+++ Physics/src/plinks.cpp
@@ -26,3 +26,31 @@ unsigned PointMassCable::addContact(PointMassContact* contact, unsigned limit) c
 
 	return 1;
 }
+
+unsigned PointMassRod::addContact(PointMassContact* contact, unsigned limit) const
+{
+	real currentLen = currentLength();
+
+	if (currentLen == length) return 0;
+
+	contact->pointMass[0] = pointMass[0];
+	contact->pointMass[1] = pointMass[1];
+
+	Vector3 normal = pointMass[1]->getPosition() - pointMass[0]->getPosition();
+	normal.normalize();
+
+	if (currentLen > length)
+	{
+		contact->contactNormal = normal;
+		contact->penetration = currentLen - length;
+	}
+	else
+	{
+		contact->contactNormal = normal * -1;
+		contact->penetration = length - currentLen;
+	}
+
+	contact->restitution = 0;
+
+	return 1;
+}
