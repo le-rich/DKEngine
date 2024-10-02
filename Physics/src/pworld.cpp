1ae132314c7d380844cd53f8e5480ceb18db5540
jharris99-git <jharris99@my.bcit.ca>
2024-10-01 23:55:04 -0700

    [JH][COM-99] Constructor and Destructor for PointMassWorld

--- Physics/src/pworld.cpp
+++ Physics/src/pworld.cpp
@@ -2,3 +2,14 @@
 
 using namespace AE86;
 
+PointMassWorld::PointMassWorld(unsigned maxContacts, unsigned iterations) : resolver(iterations), maxContacts(maxContacts)
+{
+	contacts = new PointMassContact[maxContacts];
+	calculateIterations = (iterations == 0);
+
+}
+
+PointMassWorld::~PointMassWorld()
+{
+	delete[] contacts;
+}
