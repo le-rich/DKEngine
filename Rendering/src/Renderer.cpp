74278348cddedd79aaa672952cf5260307fcd7da
Nicky Cheng <ncheng25@my.bcit.ca>
2024-10-13 15:06:06 -0700

    Modified renderer to work with textures

--- Rendering/src/Renderer.cpp
+++ Rendering/src/Renderer.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 
 #include "Renderer.h"
+#include "Resources/Texture.h"
 
 // Constructor.
 Renderer::Renderer() {}
@@ -30,7 +31,13 @@ void Renderer::Update()
     ShaderProgramSource source = ParseShader("../Rendering/Shaders/default.glsl");
     GLuint shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
+
+    Texture texture("C:\\classes\\comp8552\\DKEngine\\x64\\Debug\\Resources\\Gold_Star.png");
+    texture.Bind();
     //Apply Uniforms (lighting, view matrices, etc...)
+    std::string name = "u_Texture";
+    GLCall(int location = glGetUniformLocation(1, name.c_str()));
+    GLCall(glUniform1i(location, 0));
     /*For each Primitive
         Bind Vertex Array
         Bind Index Buffer
