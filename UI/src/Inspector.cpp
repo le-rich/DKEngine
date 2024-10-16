fb6963e91afcb2284bc0eb3b5ae857580fed32c9
Kevin Baumann <baumannkev@gmail.com>
2024-10-15 19:34:02 -0700

    Updated layout of Inspector

--- UI/src/Inspector.cpp
+++ UI/src/Inspector.cpp
@@ -12,11 +12,21 @@ void drawInspector() {
        // Section: Name
        if (ImGui::CollapsingHeader("Object", ImGuiTreeNodeFlags_DefaultOpen)) {
            ImGui::Text("Name");
+           ImGui::SameLine(75);
            static char nameBuffer[256];
            strncpy_s(nameBuffer, selectedGameObject->getName().c_str(), sizeof(nameBuffer));
            if (ImGui::InputText("##Name", nameBuffer, sizeof(nameBuffer))) {
                selectedGameObject->setName(nameBuffer);
                consoleLog("GameObject renamed to: " + std::string(nameBuffer));
+           }
+
+           ImGui::Text("Parent: ");
+           ImGui::SameLine(75);
+           if (selectedGameObject->getParent() != nullptr) {
+               ImGui::Text("%s", selectedGameObject->getParent()->getName().c_str());
+           }
+           else {
+               ImGui::Text("Root");
            }
        }
        //ImGui::Text("Selected Object: %s", selected->getName().c_str());
@@ -24,19 +34,25 @@ void drawInspector() {
        if (ImGui::CollapsingHeader("Transform", ImGuiTreeNodeFlags_DefaultOpen)) {
            // Position
            std::array<float, 3>& position = selectedGameObject->getPosition();
-           if (ImGui::DragFloat3("Position", position.data(), 0.1f)) {
+           ImGui::Text("Position");
+           ImGui::SameLine(75); // Adjust the value (100) to set appropriate spacing for label alignment
+           if (ImGui::DragFloat3("##Position", position.data(), 0.1f)) {
                selectedGameObject->setPosition(position);
            }
 
            // Rotation
            std::array<float, 3>& rotation = selectedGameObject->getRotation();
-           if (ImGui::DragFloat3("Rotation", rotation.data(), 0.1f)) {
+           ImGui::Text("Rotation");
+           ImGui::SameLine(75);
+           if (ImGui::DragFloat3("##Rotation", rotation.data(), 0.1f)) {
                selectedGameObject->setRotation(rotation);
            }
 
            // Scale
            std::array<float, 3>& scale = selectedGameObject->getScale();
-           if (ImGui::DragFloat3("Scale", scale.data(), 0.1f)) {
+           ImGui::Text("Scale");
+           ImGui::SameLine(75);
+           if (ImGui::DragFloat3("##Scale", scale.data(), 0.1f)) {
                selectedGameObject->setScale(scale);
            }
        }
