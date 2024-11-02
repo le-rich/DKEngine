5a844ffd9f5c42d8ce2c0e286ca78d1caa4a300d
jharris99-git <jharris99@my.bcit.ca>
2024-11-02 14:45:03 -0700

    Added some basic dev direction comments to AudioManager.h

--- Core/include/Managers/AudioManager.h
+++ Core/include/Managers/AudioManager.h
@@ -27,6 +27,11 @@ private:
 //    FMOD_VECTOR position = { 0.0f, 0.0f, 0.0f };
 //    FMOD_VECTOR velocity = { 0.0f, 0.0f, 0.0f };
 //};
+// 
+// I assume they need some kind of update to their shit.
+// 
+// FMOD::System::init will need to set FMOD_INIT_3D_RIGHTHANDED
+// because FMOD uses the same coordinate system as DirectX, so we need to specify Right.
 //
 //struct ListenerComponent {
 //    FMOD_VECTOR position = { 0.0f, 0.0f, 0.0f };
