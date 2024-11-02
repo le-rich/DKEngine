66eebb092eca01a88f377a11e1d486553eb26dec
jharris99-git <jharris99@my.bcit.ca>
2024-11-02 16:36:13 -0700

    Added linking for FMOD library files.

--- Core/include/Managers/AudioManager.h
+++ Core/include/Managers/AudioManager.h
@@ -1,7 +1,8 @@
 #ifndef AUDIO_MGR_H
 
-#include <fmod.h>
+#include "FMOD/inc/fmod.hpp"
 #include <string>
+#include <unordered_map>
 
 class AudioManager {
 public:
@@ -9,11 +10,21 @@ public:
 	~AudioManager();
 
 	void update();
-
 	FMOD::System* getSystem();
 
+	FMOD::Sound* loadSound(const std::string& filePath);
+
+	// IMPL fmodSystem->set3DListenerAttributes(0, &position, nullptr, &forward, &up);
+	// OR we have a private variable representing the current listener and forward
+	// to the same position as 0, not sure.
+	// Get velocity of listener so we can use it to have doppler.
+	// forward and up necessary for automated spatial audio.
+	void setListenerAttributes(const FMOD_VECTOR& position, const FMOD_VECTOR& forward, const FMOD_VECTOR& up);
+
+		void setMasterVolume(float volume);
 private:
 	FMOD::System* fmodSystem = nullptr;
+	std::unordered_map<std::string, FMOD::Sound*> soundCache;
 };
 
 // Convert this kind of structure to ECS Components
@@ -38,6 +49,9 @@ private:
 //    FMOD_VECTOR forward = { 0.0f, 0.0f, 1.0f };
 //    FMOD_VECTOR up = { 0.0f, 1.0f, 0.0f };
 //};
+// Not sure about a listener component, might just use the audio manager as the listener.
+// Alternatively, we can di
+//
 
 
 #endif
---
+++ Core/src/Managers/AudioManager.cpp
@@ -0,0 +1,48 @@
+#include "Managers/AudioManager.h"
+
+AudioManager::AudioManager()
+{
+	FMOD::System_Create(&fmodSystem);
+	fmodSystem->init(512, FMOD_INIT_3D_RIGHTHANDED, nullptr);
+}
+
+AudioManager::~AudioManager()
+{
+	for (auto& pair : soundCache)
+	{
+		pair.second->release();
+	}
+	fmodSystem->release();
+}
+
+void AudioManager::update()
+{
+	fmodSystem->update();
+}
+
+FMOD::System* AudioManager::getSystem() {
+	return fmodSystem;
+}
+
+FMOD::Sound* AudioManager::loadSound(const std::string& filePath) {
+	auto it = soundCache.find(filePath);
+	if (it != soundCache.end()) {
+		return it->second;
+	}
+
+	FMOD::Sound* newSound;
+	fmodSystem->createSound(filePath.c_str(), FMOD_3D, nullptr, &newSound);
+	soundCache[filePath] = newSound;
+	return newSound;
+}
+
+void AudioManager::setListenerAttributes(const FMOD_VECTOR& position, const FMOD_VECTOR& forward, const FMOD_VECTOR& up) {
+	FMOD_VECTOR velocity = { 0.0f, 0.0f, 0.0f }; // TODO: get velocity for doppler
+	fmodSystem->set3DListenerAttributes(0, &position, &velocity, &forward, &up);
+}
+
+void AudioManager::setMasterVolume(float volume) {
+	fmodSystem->setSoftwareFormat(0, FMOD_SPEAKERMODE_STEREO, 0);
+	fmodSystem->setSoftwareChannels(100);
+	// (Global volume changes can also be handled via channel groups, if preferred)
+}
