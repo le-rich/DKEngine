b025d86dde15b9f16ca93f0aff0b225fc79dbdca
Lukauigi <lukebednarek@hotmail.ca>
2024-11-15 12:22:42 -0800

    [LB][COM-165][COM-166] Add lap trigger in scene with detects overlap

--- Game/include/Scripts/LapColliderScript.h
+++ Game/include/Scripts/LapColliderScript.h
@@ -3,6 +3,14 @@
 #include "Script.h"
 #include "Components/TransformComponent.h"
 
+struct AABB
+{
+	glm::vec3 min;
+	glm::vec3 max;
+
+	AABB(glm::vec3 min, glm::vec3 max) : min(min), max(max) {}
+};
+
 class LapColliderScript : public Script
 {
 public:
@@ -13,6 +21,7 @@ public:
 
 	void SetSelf(TransformComponent* transform) { m_Self = transform; }
 	void SetOther(TransformComponent* transform) { m_Other = transform; }
+	bool AABBCollision(AABB* self, AABB* other);
 
 	std::unique_ptr<Script> clone() const override {
 		return std::make_unique<LapColliderScript>(*this);
@@ -20,5 +29,7 @@ public:
 private:
 	TransformComponent* m_Self = nullptr;
 	TransformComponent* m_Other = nullptr;
+	AABB* m_SelfBox;
+	AABB* m_OtherBox;
 };
 
--- Game/src/Scripts/LapColliderScript.cpp
+++ Game/src/Scripts/LapColliderScript.cpp
@@ -1,12 +1,55 @@
+#include <iostream>
+#include <string>
+
 #include "Scripts/LapColliderScript.h"
 
 LapColliderScript::LapColliderScript(Entity* mEntity) : Script(mEntity)
 {
+	m_Self = this->entity->transform;
 }
 
 LapColliderScript::~LapColliderScript() {}
 
 void LapColliderScript::Update(float deltaTime)
 {
-	// TODO: AABB
+
+
+	// make bounding box for this entity
+	glm::vec3 selfPosition = m_Self->getWorldPosition();
+	glm::vec3 boxMin = glm::vec3(selfPosition.x - 5.0f, selfPosition.y - 5.0f, selfPosition.z - 5.0f);
+	glm::vec3 boxMax = glm::vec3(selfPosition.x + 5.0f, selfPosition.y + 5.0f, selfPosition.z + 5.0f);
+	m_SelfBox = new AABB(boxMin, boxMax);
+
+	//std::cout << "Lap Checkpoint Bounding Box: " << std::to_string(m_Other->getWorldPosition()) << std::endl;
+
+	// make bounding box for car entity
+	glm::vec3 otherPosition = m_Other->getWorldPosition();
+	boxMin = glm::vec3(otherPosition.x - 5.0f, otherPosition.y - 5.0f, otherPosition.z - 5.0f);
+	boxMax = glm::vec3(otherPosition.x + 5.0f, otherPosition.y + 5.0f, otherPosition.z + 5.0f);
+	m_OtherBox = new AABB(boxMin, boxMax);
+
+	if (AABBCollision(m_SelfBox, m_OtherBox))
+	{
+		std::cout << "Overlap detected!!!" << std::endl;
+	}
+	else
+	{
+		std::cout << "No Overlap" << std::endl;
+	}
+}
+
+bool LapColliderScript::AABBCollision(AABB* self, AABB* other)
+{
+	
+	float d1x = other->min.x - self->max.x;
+	float d1y = other->min.y - self->max.y;
+	float d2x = self->min.x - other->max.x;
+	float d2y = self->min.y - other->max.y;
+
+	if (d1x > 0.0f || d1y > 0.0f)
+		return false;
+	if (d2x > 0.0f || d2y > 0.0f)
+		return false;
+
+	return true;
 }
