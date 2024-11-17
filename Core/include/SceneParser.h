6575e882bb6e62e4027fa833ba2385edb4b93a5c
Kyle N <kyle.ng7512@gmail.com>
2024-11-17 02:07:14 -0800

    [KN][COM-177] Add exception handling.

--- Core/include/SceneParser.h
+++ Core/include/SceneParser.h
@@ -18,31 +18,55 @@ namespace SceneParser
     using json = nlohmann::json;
     enum SceneElement
     {
-        assets,
-        entities
+        ASSETS = 0,
+        ENTITIES
     };
+
+    static const std::string ASSET_KEY = "assets";
+    static const std::string ENTITY_KEY = "entities";
 
     std::unordered_map<std::string, SceneElement> const ElementMap = {
-        {"assets", SceneElement::assets},
-        {"entities", SceneElement::entities}
+        {ASSET_KEY, SceneElement::ASSETS},
+        {ENTITY_KEY, SceneElement::ENTITIES}
     };
 
-    Transform getTransform(json& transformObject)
+    void ParseTransform(Entity* pEntity, json& transformObject)
     {
         Transform transform;
-        std::vector<float> localPosition;
-        transformObject.at("localPosition").get_to(localPosition);
-        transform.localPosition = (localPosition.size() > 0) ? glm::vec3(localPosition[0], localPosition[1], localPosition[2]) : glm::vec3();
+        try
+        {
+            std::vector<float> localPosition;
+            transformObject.at("localPosition").get_to(localPosition);
+            transform.localPosition = (localPosition.size() > 0) ? glm::vec3(localPosition[0], localPosition[1], localPosition[2]) : glm::vec3();
+        }
+        catch (json::out_of_range& e)
+        {
+            std::cout << e.what() << '\n';
+        }
 
-        std::vector<float> localOrientation;
-        transformObject.at("localOrientation").get_to(localOrientation);
-        transform.localOrientation = (localOrientation.size() > 0) ? glm::quat(localOrientation[0], localOrientation[1], localOrientation[2], localOrientation[3]) : glm::quat(1.f, 0, 0, 0);
+        try
+        {
+            std::vector<float> localOrientation;
+            transformObject.at("localOrientation").get_to(localOrientation);
+            transform.localOrientation = (localOrientation.size() > 0) ? glm::quat(localOrientation[0], localOrientation[1], localOrientation[2], localOrientation[3]) : glm::quat(1.f, 0, 0, 0);
+        }
+        catch (json::out_of_range& e)
+        {
+            std::cout << e.what() << '\n';
+        }
 
-        std::vector<float> localScale;
-        transformObject.at("localScale").get_to(localScale);
-        transform.localScale = (localScale.size() > 0) ? glm::vec3(localScale[0], localScale[1], localScale[2]) : glm::vec3(1.f);
+        try
+        {
+            std::vector<float> localScale;
+            transformObject.at("localScale").get_to(localScale);
+            transform.localScale = (localScale.size() > 0) ? glm::vec3(localScale[0], localScale[1], localScale[2]) : glm::vec3(1.f);
+        }
+        catch (json::out_of_range& e)
+        {
+            std::cout << e.what() << '\n';
+        }
 
-        return transform;
+        pEntity->transform->setTransform(transform);
     }
 
     void getComponents(Entity* pEntity, json& pComponentObjects)
@@ -65,106 +89,150 @@ namespace SceneParser
             ComponentType type = componentMapIt->second;
             switch (type)
             {
-            case ComponentType::Camera:
-            {
-                // TODO: Check for missing fields
-                CameraComponent* cameraComponent = new CameraComponent(pEntity);
-                pEntity->addComponent(*cameraComponent);
-                params.at("fieldOfView").get_to(cameraComponent->fieldOfView);
-                params.at("farClipPlane").get_to(cameraComponent->farClipPlane);
-                params.at("nearClipPlane").get_to(cameraComponent->nearClipPlane);
-                
-            }
-            break;
-            case ComponentType::Light:
-            {
-                LightComponent* lightComponent = new LightComponent(pEntity);
-                pEntity->addComponent(*lightComponent);
-
-                LightParams lightParams;
-                std::vector<float> color;
-                params.at("color").get_to(color);
-                lightParams.color = (color.size() > 0) ? glm::vec4(color[0], color[1], color[2], color[3]) : glm::vec4(1.f);
-                
-                std::string lightTypeString;
-                params.at("type").get_to(lightTypeString);
-                auto lightMapIt = LightTypeMap.find(lightTypeString);
-                if (lightMapIt == LightTypeMap.end())
+                case ComponentType::Camera:
                 {
-                    std::cout << "ERROR: " << lightTypeString << " not valid LightType" << std::endl;
+                    // TODO: Check for missing fields
+                    CameraComponent* cameraComponent = new CameraComponent(pEntity);
+                    pEntity->addComponent(*cameraComponent);
+                    params.at("fieldOfView").get_to(cameraComponent->fieldOfView);
+                    params.at("farClipPlane").get_to(cameraComponent->farClipPlane);
+                    params.at("nearClipPlane").get_to(cameraComponent->nearClipPlane);
+                    break;
                 }
-                lightParams.type = lightMapIt->second;
+                case ComponentType::Light:
+                {
+                    LightComponent* lightComponent = new LightComponent(pEntity);
+                    pEntity->addComponent(*lightComponent);
 
-                params.at("intensity").get_to(lightParams.intensity);
-                params.at("constant").get_to(lightParams.constant);
-                params.at("linear").get_to(lightParams.linear);
-                params.at("quadratic").get_to(lightParams.quadratic);
-                params.at("cutoff").get_to(lightParams.cutoff);
-                params.at("outercutoff").get_to(lightParams.outercutoff);
+                    LightParams lightParams;
+                    std::vector<float> color;
+                    params.at("color").get_to(color);
+                    lightParams.color = (color.size() > 0) ? glm::vec4(color[0], color[1], color[2], color[3]) : glm::vec4(1.f);
 
-                lightComponent->SetParams(lightParams);
+                    std::string lightTypeString;
+                    params.at("type").get_to(lightTypeString);
+                    auto lightMapIt = LightTypeMap.find(lightTypeString);
+                    if (lightMapIt == LightTypeMap.end())
+                    {
+                        std::cout << "ERROR: " << lightTypeString << " not valid LightType" << std::endl;
+                    }
+                    lightParams.type = lightMapIt->second;
+
+                    params.at("intensity").get_to(lightParams.intensity);
+                    params.at("constant").get_to(lightParams.constant);
+                    params.at("linear").get_to(lightParams.linear);
+                    params.at("quadratic").get_to(lightParams.quadratic);
+                    params.at("cutoff").get_to(lightParams.cutoff);
+                    params.at("outercutoff").get_to(lightParams.outercutoff);
+
+                    lightComponent->SetParams(lightParams);
+                }
+                break;
+                case ComponentType::Script:
+                    break;
+                default:
+                    break;
             }
-                break;
-            case ComponentType::Script:
-                break;
-            default:
-                break;
+        }
+    }
+
+    void ParseAssets(json& pAssetObjects)
+    {
+        for (auto& asset : pAssetObjects)
+        {
+            try
+            {
+                auto folder = asset.at("folder");
+                auto file = asset.at("file");
+
+                Entity* entity = new Entity();
+                GLTFLoader::LoadModelAsEntity(entity, folder, file);
+                EntityManager::getInstance().Instantiate(entity);
+            }
+            catch (const json::out_of_range& e)
+            {
+                std::cout << e.what() << '\n';
+                std::cout << asset << '\n';
+            }
+        }
+    }
+
+    void ParseEntities(json& pEntityObjects)
+    {
+        for (auto& entityElement : pEntityObjects)
+        {
+            Entity* entity = nullptr;
+            try
+            {
+                std::string name = entityElement.at("name").template get<std::string>();
+                entity = EntityManager::getInstance().findFirstEntityByDisplayName(name);
+                if (entity == nullptr)
+                {
+                    entity = new Entity(name);
+                    EntityManager::getInstance().Instantiate(entity);
+                }
+
+            }
+            catch (const json::out_of_range& e)
+            {
+                std::cout << e.what() << '\n';
+                entity = new Entity();
+                EntityManager::getInstance().Instantiate(entity);
+            }
+            try
+            {
+                // Parse Transform
+                json transformObject = entityElement.at("transform");
+                ParseTransform(entity, transformObject);
+
+            }
+            catch (const json::out_of_range& e)
+            {
+                std::cout << e.what() << '\n';
+            }
+            try
+            {
+                // Parse Components
+                json components = entityElement.at("components");
+                getComponents(entity, components);
+            }
+            catch (const json::out_of_range& e)
+            {
+                std::cout << e.what() << '\n';
             }
         }
     }
 
     static void LoadScene(std::string pSceneFilePath)
     {
-        std::ifstream i;
-        i.open(pSceneFilePath);
-        json j;
-        i >> j;
-        for (auto& element : j.items())
+        std::ifstream inputStream;
+        inputStream.open(pSceneFilePath);
+        if (!inputStream.is_open())
         {
-            auto it = ElementMap.find(element.key());
-            if (it == ElementMap.end())
-            {
-                std::cout << "ERROR: " << element.key() << " not valid Scene Element" << std::endl;
-                continue;
-            }
-            switch (it->second)
-            {
-            case SceneElement::assets:
-                for (auto& asset : element.value().items())
-                {
-                    auto folder = asset.value().find("folder");
-                    auto file = asset.value().find("file");
+            std::cout << "ERROR: " << pSceneFilePath << " does not exist!" << '\n';
+            return;
+        }
 
-                    Entity* entity = new Entity();
-                    GLTFLoader::LoadModelAsEntity(entity, folder.value(), file.value());
-                    EntityManager::getInstance().Instantiate(entity);
-                }
-                break;
-            case SceneElement::entities:
-                for (auto& entityElement : element.value().items())
-                {
-                    std::string name;
-                    entityElement.value().at("name").get_to(name);
-                    json transformObject = entityElement.value().at("transform");
-                    json components = entityElement.value().at("components");
+        json sceneJSON;
+        inputStream >> sceneJSON;
+        try
+        {
+            json assetObjects = sceneJSON.at(ASSET_KEY);
+            ParseAssets(assetObjects);
+        }
+        catch (const json::out_of_range& e)
+        {
+            std::cout << e.what() << '\n';
+        }
 
-                    Entity* entity = EntityManager::getInstance().findFirstEntityByDisplayName(name);
-                    if (entity == nullptr)
-                    {
-                        entity = new Entity(name);
-                        EntityManager::getInstance().Instantiate(entity);
-                    }
-
-                    // Parse Transform
-                    Transform transform = getTransform(transformObject);
-                    entity->transform->setTransform(transform);
-
-                    // Parse Components
-                    getComponents(entity, components);
-
-                }
-                break;
-            }
+        try
+        {
+            json entitiesObject = sceneJSON.at(ENTITY_KEY);
+            ParseEntities(entitiesObject);
+        }
+        catch (const json::out_of_range& e)
+        {
+            std::cout << e.what() << '\n';
         }
     }
 }
