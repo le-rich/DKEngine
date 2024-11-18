203e44e7badeb8ed0d06d68f29607d65319ed570
Sepehrman <sepehrman@icloud.com>
2024-11-17 17:57:36 -0800

    [SM][com-137] Resolved dependency issues with FMOD

---
+++ Core/include/Managers/AudioManager.h
@@ -0,0 +1,71 @@
+﻿#ifndef AUDIO_MGR_H
+#define AUDIO_MGR_H
+
+#include "System.h"
+#include "FMOD/inc/fmod.hpp"
+#include <string>
+#include <unordered_map>
+
+class AudioManager : public System {
+public:
+	AudioManager();
+	~AudioManager();
+
+	void Update();
+
+	const char* GetName() const override {
+		return "AudioManager";
+	}
+
+	void FixedUpdate() override {
+		// @Richard & @Sepehr: Not sure what we want here, maybe nothing
+	}
+
+	FMOD::System* getSystem();
+
+	FMOD::Sound* loadSound(const std::string& filePath);
+
+	// IMPL fmodSystem->set3DListenerAttributes(0, &position, nullptr, &forward, &up);
+	// OR we have a private variable representing the current listener and forward
+	// to the same position as 0, not sure.
+	// Get velocity of listener so we can use it to have doppler.
+	// forward and up necessary for automated spatial audio.
+	void setListenerAttributes(const FMOD_VECTOR& position, const FMOD_VECTOR& forward, const FMOD_VECTOR& up);
+
+	void setMasterVolume(float volume);
+
+	void playSound(const std::string& soundName);
+
+private:
+	FMOD::System* fmodSystem = nullptr;
+	std::unordered_map<std::string, FMOD::Sound*> soundCache;
+};
+
+// Convert this kind of structure to ECS Components
+//struct AudioComponent {
+//    std::string filePath;
+//    FMOD::Sound* sound = nullptr;
+//    FMOD::Channel* channel = nullptr;
+//    bool isPlaying = false;
+//
+//    // 3D sound attributes
+//    FMOD_VECTOR position = { 0.0f, 0.0f, 0.0f };
+//    FMOD_VECTOR velocity = { 0.0f, 0.0f, 0.0f };
+//};
+// 
+// I assume they need some kind of update to their shit.
+// 
+// FMOD::System::init will need to set FMOD_INIT_3D_RIGHTHANDED
+// because FMOD uses the same coordinate system as DirectX, so we need to specify Right.
+//
+//struct ListenerComponent {
+//    FMOD_VECTOR position = { 0.0f, 0.0f, 0.0f };
+//    FMOD_VECTOR forward = { 0.0f, 0.0f, 1.0f };
+//    FMOD_VECTOR up = { 0.0f, 1.0f, 0.0f };
+//};
+// Not sure about a listener component, might just use the audio manager as the listener.
+// Alternatively, we can di
+//
+
+
+#endif // AUDIO_MGR_H
---
+++ Core/src/Managers/AudioManager.cpp
@@ -0,0 +1,60 @@
+﻿#include "Managers/AudioManager.h"
+#include "iostream"
+
+AudioManager::AudioManager()
+{
+    FMOD::System_Create(&fmodSystem);
+    fmodSystem->init(512, FMOD_INIT_3D_RIGHTHANDED, nullptr);
+}
+
+AudioManager::~AudioManager()
+{
+    for (auto& pair : soundCache)
+    {
+        pair.second->release();
+    }
+    fmodSystem->release();
+}
+
+void AudioManager::Update()
+{
+    fmodSystem->update();
+}
+
+FMOD::System* AudioManager::getSystem() {
+    return fmodSystem;
+}
+
+FMOD::Sound* AudioManager::loadSound(const std::string& filePath) {
+    auto it = soundCache.find(filePath);
+    if (it != soundCache.end()) {
+        return it->second;
+    }
+
+    FMOD::Sound* newSound;
+    fmodSystem->createSound(filePath.c_str(), FMOD_3D, nullptr, &newSound);
+    soundCache[filePath] = newSound;
+    return newSound;
+}
+
+void AudioManager::setListenerAttributes(const FMOD_VECTOR& position, const FMOD_VECTOR& forward, const FMOD_VECTOR& up) {
+    FMOD_VECTOR velocity = { 0.0f, 0.0f, 0.0f }; // TODO: get velocity for doppler
+    fmodSystem->set3DListenerAttributes(0, &position, &velocity, &forward, &up);
+}
+
+void AudioManager::setMasterVolume(float volume) {
+    fmodSystem->setSoftwareFormat(0, FMOD_SPEAKERMODE_STEREO, 0);
+    fmodSystem->setSoftwareChannels(100);
+}
+
+void AudioManager::playSound(const std::string& soundName) {
+
+	
+    auto it = soundCache.find(soundName);
+    if (it != soundCache.end()) {
+        FMOD::Channel* newChannel = nullptr;
+        fmodSystem->playSound(it->second, nullptr, false, &newChannel);
+    } else {
+        std::cerr << "Sound not found: " << soundName << std::endl;
+    }
+}
