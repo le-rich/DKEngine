9c758dcc9318c15d64a7131149900bfee6db0051
Lukauigi <lukebednarek@hotmail.ca>
2024-11-19 17:10:17 -0800

    [LB] Remove commented out code

--- Game/src/Scripts/LapCheckpointScript.cpp
+++ Game/src/Scripts/LapCheckpointScript.cpp
@@ -16,16 +16,12 @@ void LapCheckpointScript::SetLapManager(LapManagerScript* lapManager)
 
 void LapCheckpointScript::Update(float deltaTime)
 {
-	//if (!mParams.m_Self || !mParams.m_Other) { return; }
-	//// Sets BB correctly even after setWorldPosition is used on Entity in Scene
-	//if (!mParams.m_SelfBB) { mParams.m_SelfBB = new AE86::Aabb(glm::vec3(mParams.m_Self->getWorldPosition()), mParams.m_BBOffset); }
 
 	mParams.m_OtherBB->UpdateBounds(mParams.m_Other->getWorldPosition());
 
 	if (!mParams.m_Registered && mParams.m_SelfBB->CheckCollision(*mParams.m_OtherBB))
 	{
 		mParams.m_LapManager->OnCheckpointTriggered(mParams.m_Index);
-		//m_OnCheckpointCallback(mParams.m_Index);
 	}
 }
 
