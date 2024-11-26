6d64f1f83c22bf63e5c188a13c272f52f1bc6c4b
Kyledude252 <kyleowena@gmail.com>
2024-11-25 17:20:13 -0800

    [KA][COM-232] Camera Component UI
    Also changed the spacing of the inspector elements so they do not get pushed off the right side of the inspector at smaller sizes

--- UI/src/Inspector.cpp
+++ UI/src/Inspector.cpp
@@ -81,7 +81,6 @@ void drawInspector(Scene* scene) {
              break;
           }
        }
-       //TODO: Check for which components an entity has and call the approprite function to draw
        
    }
    else {
@@ -94,28 +93,35 @@ void drawTransformComponent() {
    if (ImGui::CollapsingHeader("Transform", ImGuiTreeNodeFlags_DefaultOpen)) {
       TransformComponent* transform = getSelectedEntity()->transform;
       if (transform != nullptr) {
+         int sectionWidth = 75;
 
          // Position
+         ImGui::SetNextItemWidth(sectionWidth);
          glm::vec3 position = transform->getLocalPosition();
          ImGui::Text("Position");
-         ImGui::SameLine(75); // Adjust the value (100) to set appropriate spacing for label alignment
+         ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+         ImGui::SameLine(sectionWidth);
          if (ImGui::DragFloat3("##Position", glm::value_ptr(position), 0.1f)) {
             transform->setLocalPosition(position);
          }
 
          // Rotation
+         ImGui::SetNextItemWidth(sectionWidth);
          glm::vec3 rotation = glm::degrees(glm::eulerAngles(transform->getLocalOrientation()));
          ImGui::Text("Rotation");
-         ImGui::SameLine(75);
+         ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+         ImGui::SameLine(sectionWidth);
          if (ImGui::DragFloat3("##Rotation", glm::value_ptr(rotation), 0.1f)) {
 
             transform->setLocalOrientation(glm::quat(glm::radians(rotation)));
          }
 
          // Scale
+         ImGui::SetNextItemWidth(sectionWidth);
          glm::vec3 scale = transform->getLocalScale();
          ImGui::Text("Scale");
-         ImGui::SameLine(75);
+         ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+         ImGui::SameLine(sectionWidth);
          if (ImGui::DragFloat3("##Scale", glm::value_ptr(scale), 0.1f)) {
             transform->setLocalScale(scale);
          }
@@ -126,54 +132,69 @@ void drawTransformComponent() {
 void drawLightComponent(Component* component) {
    if (ImGui::CollapsingHeader("Light", ImGuiTreeNodeFlags_DefaultOpen)) {
       LightComponent* light = static_cast<LightComponent*>(component);
+      int sectionWidth = 100;
 
+      ImGui::SetNextItemWidth(sectionWidth);
       const char* types[] = { "Ambient", "Point", "Directional", "Spot", "Area" };
       LightType lightType = light->GetType();
       int index = static_cast<int>(lightType);
       ImGui::Text("Type");
-      ImGui::SameLine(100);
+      ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+      ImGui::SameLine(sectionWidth);
       if (ImGui::Combo("##Type", &index, types, 5)) {
          light->SetType(static_cast<LightType>(index));
       }
 
+      ImGui::SetNextItemWidth(sectionWidth);
       float intensity = light->GetIntensity();
       ImGui::Text("Intensity");
-      ImGui::SameLine(100);
+      ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+      ImGui::SameLine(sectionWidth);
       if (ImGui::DragFloat("##Intensity", &intensity, 0.1f, 0.0f)) {
          light->SetIntensity(intensity);
       }
 
+      ImGui::SetNextItemWidth(sectionWidth);
       float constant = light->GetConstant();
       ImGui::Text("Constant");
-      ImGui::SameLine(100);
+      ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+      ImGui::SameLine(sectionWidth);
       if (ImGui::DragFloat("##Constant", &constant, 0.1f)) {
          light->SetConstant(constant);
       }
 
+      ImGui::SetNextItemWidth(sectionWidth);
       float linear = light->GetLinear();
       ImGui::Text("Linear");
-      ImGui::SameLine(100);
+      ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+      ImGui::SameLine(sectionWidth);
       if (ImGui::DragFloat("##Linear", &linear, 0.1f)) {
          light->SetLinear(linear);
       }
 
+      ImGui::SetNextItemWidth(sectionWidth);
       float quadratic = light->GetQuadratic();
       ImGui::Text("Quadratic");
-      ImGui::SameLine(100);
+      ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+      ImGui::SameLine(sectionWidth);
       if (ImGui::DragFloat("##Quadratic", &quadratic, 0.1f)) {
          light->SetQuadratic(quadratic);
       }
 
+      ImGui::SetNextItemWidth(sectionWidth);
       float cutoff = light->GetCutoff();
       ImGui::Text("Cutoff");
-      ImGui::SameLine(100);
+      ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+      ImGui::SameLine(sectionWidth);
       if (ImGui::DragFloat("##Cutoff", &cutoff, 0.1f)) {
          light->SetCutoff(cutoff);
       }
 
+      ImGui::SetNextItemWidth(sectionWidth);
       float outerCutoff = light->GetOuterCutoff();
       ImGui::Text("Outer Cutoff");
-      ImGui::SameLine(100);
+      ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+      ImGui::SameLine(sectionWidth);
       if (ImGui::DragFloat("##OuterCutoff", &outerCutoff, 0.1f)) {
          light->SetOuterCutoff(outerCutoff);
       }
@@ -182,6 +203,7 @@ void drawLightComponent(Component* component) {
 void drawMeshComponent(Component* component) {
    if (ImGui::CollapsingHeader("Mesh", ImGuiTreeNodeFlags_DefaultOpen)) {
       MeshComponent* mesh = static_cast<MeshComponent*>(component);
+      int sectionWidth = 100;
    }
 }
 
@@ -196,6 +218,43 @@ void drawCameraComponent(Component* component)
 {
    if (ImGui::CollapsingHeader("Camera", ImGuiTreeNodeFlags_DefaultOpen)) {
       CameraComponent* camera = static_cast<CameraComponent*>(component);
+      int sectionWidth = 115;
+
+      ImGui::SetNextItemWidth(sectionWidth);
+      float fov = camera->getFieldOfView();
+      ImGui::Text("Field Of View");
+      ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+      ImGui::SameLine(sectionWidth);
+      if (ImGui::DragFloat("##fov", &fov, 0.1f)) {
+         camera->setFieldOfView(fov);
+      }
+
+      ImGui::SetNextItemWidth(sectionWidth);
+      float fcp = camera->getFarClipPlane();
+      ImGui::Text("Far Clip Plane");
+      ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+      ImGui::SameLine(sectionWidth);
+      if (ImGui::DragFloat("##fcp", &fcp, 0.1f)) {
+         camera->setFarClipPlane(fcp);
+      }
+
+      ImGui::SetNextItemWidth(sectionWidth);
+      float ncp = camera->getNearClipPlane();
+      ImGui::Text("Near Clip Plane");
+      ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+      ImGui::SameLine(sectionWidth);
+      if (ImGui::DragFloat("##ncp", &ncp, 0.1f)) {
+         camera->setNearClipPlane(ncp);
+      }
+
+      ImGui::SetNextItemWidth(sectionWidth);
+      float ar = camera->getAspectRatio();
+      ImGui::Text("Aspect Ratio");
+      ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - sectionWidth);
+      ImGui::SameLine(sectionWidth);
+      if (ImGui::DragFloat("##ar", &ar, 0.1f)) {
+         camera->setAspectRatio(ar);
+      }
    }
 }
 
@@ -203,5 +262,6 @@ void drawScriptComponent(Component* component)
 {
    if (ImGui::CollapsingHeader("Script", ImGuiTreeNodeFlags_DefaultOpen)) {
       ScriptComponent* script = static_cast<ScriptComponent*>(component);
+      int sectionWidth = 100;
    }
 }
