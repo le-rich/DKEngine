55566bd7ccfcc7bab0783ff566915e527cdcc1d0
jharris99-git <jharris99@my.bcit.ca>
2024-11-29 02:20:01 -0800

    [COM-245][JH] Implemented PlaySound for AudioComponent

--- Core/AudioComponent.cpp
+++ Core/AudioComponent.cpp
@@ -40,7 +40,16 @@ AudioComponent::~AudioComponent() {
 };
 
 void AudioComponent::PlaySound(FMOD::Sound* sound, bool isLooping, bool isPaused) {
-
+	if (!sound) return;
+	if (isLooping)
+		sound->setMode(FMOD_LOOP_NORMAL);
+	FMOD_RESULT result = am->GetSystem()->playSound(sound, group, true, &channel);
+	if (result == FMOD_OK && channel) {
+		channel->setLoopCount(-1);
+		FMOD_VECTOR pos = AudioManager::GetFMODVector(entity->transform->getWorldPosition());
+		channel->set3DAttributes(&pos, nullptr);
+		channel->setPaused(isPaused);
+	}
 };
 
 void AudioComponent::update(float deltaTime) {
